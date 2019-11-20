9cb8a0062d721421e4790431604adc9604229976
Shailendra Kushwaha <learning4succedding@gmail.com>
2019-11-21 01:16:17 +0200

    added changes

--- src/assets/data/work_data.js
+++ src/assets/data/work_data.js
@@ -1,14 +1,32 @@
-import imageviewer from "../images/work/imageviewer.png";
+import vidly from "../images/work/vidly.png";
+import counter from "../images/work/counter.png";
 import github from "../images/work/github.png";
 import todoList from "../images/work/todoList.png";
+import sky from "../images/work/sky.png";
 
 export default [
+  {
+    name: "Vidly-Project",
+    info: " Get your favourite movies.",
+    img: vidly,
+    alt: "vidly",
+    codeLink: "https://github.com/Shailendra9k/vidly-project",
+    demoLink: "https://vidlye.netlify.com/"
+  },
+  {
+    name: "Counter-App",
+    info: " Performs basic calculation just like shopping cart.",
+    img: counter,
+    alt: "counter-app",
+    codeLink: "https://github.com/Shailendra9k/counter-app",
+    demoLink: "https://countr.netlify.com/"
+  },
   {
     name: "GitHub Rest Api fetch",
     info:
       " An app where you provide keyword to get github repositories for that and you are able to sort based on name, url and owner of that repositories.",
     img: github,
-    alt: "chatapp",
+    alt: "github",
     codeLink: "https://github.com/Shailendra9k/GitHub-REST-API",
     demoLink: "https://repositories.netlify.com/"
   },
@@ -19,5 +37,22 @@ export default [
     alt: "chatapp",
     codeLink: "https://github.com/Shailendra9k/to-do-starter",
     demoLink: "https://todolist-starter.herokuapp.com/"
+  },
+  {
+    name: "sky-counter",
+    info: "Check out your favourite books.",
+    img: sky,
+    alt: "sky",
+    codeLink: "https://github.com/Shailendra9k/to-do-starter",
+    demoLink: "https://github.com/Shailendra9k/sky-counter"
+  },
+  {
+    name: "react-router",
+    info:
+      "Browser throuhg various links from one page to another on different tabs.",
+    img: router,
+    alt: "react-reuter",
+    codeLink: "https://github.com/Shailendra9k/React-Routing",
+    demoLink: "https://react-routr.netlify.com/"
   }
 ];
